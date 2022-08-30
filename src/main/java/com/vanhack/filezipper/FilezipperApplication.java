b1caf935c1c4fb7d48eb48b578c6b654b711185f
gzarazaga <gzarazaga@gmail.com>
2022-08-29 22:38:43 -0300

    Initalizing project

---
+++ src/main/java/com/vanhack/filezipper/FilezipperApplication.java
@@ -0,0 +1,13 @@
+package com.vanhack.filezipper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilezipperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilezipperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vanhack/filezipper/FilezipperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanhack.filezipper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilezipperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
